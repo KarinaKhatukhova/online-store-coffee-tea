24be0adf49d74b3bfb7c67174c588bc3f8d0bec7
karina.khatukhova <startony250@gmail.com>
2026-04-02 16:49:18 +0300

    Add-all-files

---
+++ src/ru/store/online/model/enumeration/Country.java
@@ -0,0 +1,5 @@
+package ru.store.online.model.enumeration;
+
+public enum Country {
+    BRAZIL, INDIA, CHINA, ARGENTINA, GEORGIA, MEXICO, SPAIN
+}
---
+++ src/ru/store/online/model/enumeration/Status.java
@@ -0,0 +1,6 @@
+package ru.store.online.model.enumeration;
+
+public enum Status {
+
+    NEW, PAYED, CANCELED, APPROVED, DONE
+}
